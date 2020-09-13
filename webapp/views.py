54d07f6abb88dedb983843f4620f4f31f1ec2e4e
Jacob Brasil <jnxtbrasil@gmail.com>
2020-09-13 01:37:40 -0400

    Added Views

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets, views
+from .models import Student, Camera
+from .serializers import StudentSerializer, CameraSerializer
+from recognize import Recognition
 
 # Create your views here.
+
+def FaceFound(name):
+    print(name)
+    # Do code for websockets here
+
+class StudentView(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    Recognition.train("media", "trained_knn_model.clf")
+
+class CameraView(viewsets.ModelViewSet):
+    queryset = Camera.objects.all()
+    serializer_class = CameraSerializer
+
+class Run(views.APIView):
+    def put(self, request, pk, format=None):
+        Recognition.run_recognition(Camera.objects.first().address, FaceFound)
+
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+    
